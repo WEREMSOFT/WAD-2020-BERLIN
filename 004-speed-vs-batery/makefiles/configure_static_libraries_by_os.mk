434cc78beaacf72666e367cf265d2b427166a67f
WEREMSOFT <pablo.weremczuk@gmail.com>
2020-05-02 20:06:40 +0200

    [ADD] make build for macos

--- 004-speed-vs-batery/makefiles/configure_static_libraries_by_os.mk
+++ 004-speed-vs-batery/makefiles/configure_static_libraries_by_os.mk
@@ -2,7 +2,7 @@
 ifeq ($(DETTECTED_OS),Linux)
 	LINK_LIBS := -l:libraylib-linux.a -l:libglfw3.a -lm -ldl -lpthread -lX11 -lxcb -lGL -lGLX -lXext -lGLdispatch -lXau -lXdmcp
 else ifeq ($(DETTECTED_OS),Darwin)
-	LINK_LIBS := -O0 -framework IOKit -v 
+	LINK_LIBS := -O0 -framework IOKit -v -lraylib -framework OpenGL -framework cocoa 
 	# find_library(LIB_GLUT GLUT)
     # find_library(OPEN_GL_LIBRARY OpenGL)
     # find_library(COCOA_LIBRARY Cocoa)
